b492a3c92ee444aee3a47c640027fdd5712b528a
hanyongjun <gksdydwns34@naver.com>
2023-05-25 16:25:41 +0900

    댓글 작성 logic

--- Project/src/main/java/com/myproject/myapp/ReplyController.java
+++ Project/src/main/java/com/myproject/myapp/ReplyController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 import com.myproject.model.ReplyVO;
 import com.myproject.service.ReplyService;
 
@@ -13,22 +14,13 @@ import com.myproject.service.ReplyService;
 @RequestMapping("/reply/*")
 public class ReplyController {
 
-	
 	 @Autowired
 	 private ReplyService service;
-	 
-	 //댓글 조회
-	 
+	  
 	 //댓글 작성
 	 @RequestMapping(value = "/write", method = RequestMethod.POST)
 	 public String postreWirte(ReplyVO vo, Model model) throws Exception {		 
 		 service.replyinsert(vo);
 		 return "redirect:/board/detail?bno=" + vo.getBno();
-	 }
-	 
-	 //댓글 수정
-	 
-	 
-	 //댓글 삭제
-	
+	 }	 
 }
